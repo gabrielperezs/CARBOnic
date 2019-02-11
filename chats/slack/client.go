82fa96d8c3e3a035a53ff9e82c565cd81db0954c
Gabriel Pérez S <gabriel@guero.net>
2019-02-11 12:28:36 +0100

    FIX: remove unnecessary message when is not coming from a group. And other small fixes.

--- chats/slack/client.go
+++ chats/slack/client.go
@@ -12,9 +12,7 @@ import (
 )
 
 func NewConnection(token string) (*SlackBOT, error) {
-
 	b := &SlackBOT{}
-
 	if err := b.connect(token); err != nil {
 		return nil, err
 	}
@@ -22,7 +20,6 @@ func NewConnection(token string) (*SlackBOT, error) {
 	b.rtm = b.bot.NewRTM()
 	go b.rtm.ManageConnection()
 	go b.listener()
-
 	return b, nil
 }
 
@@ -34,74 +31,58 @@ type SlackBOT struct {
 	r   []*Slack
 }
 
-func (tb *SlackBOT) connect(token string) error {
-
-	var err error
-
+func (tb *SlackBOT) connect(token string) (err error) {
 	tb.bot = slack.New(token)
-
 	tb.me, err = tb.bot.AuthTest()
 	if err != nil {
 		log.Printf("ERROR: Slack AuthTest: %s", err)
 		return err
 	}
-
 	log.Printf("Slack Authorized on account %s", tb.me.User)
-
 	return nil
 }
 
 func (tb *SlackBOT) listener() {
-
 	for msg := range tb.rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
-
 			if ev.Msg.User == "" {
 				continue
 			}
-
 			if ev.User == tb.me.User {
-				log.Printf("D: ignore my self")
 				continue
 			}
-
-			group := false
 			for _, t := range tb.r {
-				if t.cfg.Group == ev.Msg.Channel {
-					group = true
-					if lib.IsDupMessage(fmt.Sprintf("slack_cmd_%s", ev.Channel), ev.Text) {
-						continue
-					}
-					info, err := tb.rtm.GetUserInfo(ev.User)
-					if err != nil {
-						cmds.Commands(t, "unknown", ev.Text)
-					}
-					var name string
-					if info.Profile.DisplayName == "" {
-						name = info.Profile.RealName
-					} else {
-						name = info.Profile.DisplayName
-					}
-					cmds.Commands(t, name, ev.Text)
+				if t.cfg.Group != ev.Msg.Channel {
+					// Ignore message that are not comming from the configured channels/groups
+					continue
 				}
+				if lib.IsDupMessage(fmt.Sprintf("slack_cmd_%s", ev.Channel), ev.Text) {
+					// Ignore duplicate messages
+					continue
+				}
+				info, err := tb.rtm.GetUserInfo(ev.User)
+				if err != nil {
+					// If is an unknown user just define as "unknown" an continue
+					// because this is consider as an error
+					cmds.Commands(t, fmt.Sprintf("[unknown:%s]", err), ev.Text)
+					continue
+				}
+				name := info.Profile.DisplayName
+				if name == "" {
+					name = info.Profile.RealName
+				}
+				cmds.Commands(t, name, ev.Text)
 			}
-
-			if !group {
-				go tb.rtm.PostMessage(ev.Msg.Channel, slack.MsgOptionText("Can you read?", false))
-			}
-
 		case *slack.RTMError:
 			log.Printf("Slack ERROR: %s", ev.Error())
 		}
 	}
-
 }
 
 func (tb *SlackBOT) Send(g string, message *lib.Message) error {
-	var err error
 	if message.Score > 5 {
-		_, _, err = tb.rtm.PostMessage(
+		_, _, err := tb.rtm.PostMessage(
 			g,
 			slack.MsgOptionText(message.Msg, false),
 			slack.MsgOptionAttachments(
@@ -111,14 +92,8 @@ func (tb *SlackBOT) Send(g string, message *lib.Message) error {
 				},
 			),
 		)
-	} else {
-		_, _, err = tb.rtm.PostMessage(
-			g,
-			slack.MsgOptionText(message.Msg, false),
-		)
-	}
-	if err != nil {
 		return err
 	}
-	return nil
+	_, _, err := tb.rtm.PostMessage(g, slack.MsgOptionText(message.Msg, false))
+	return err
 }
